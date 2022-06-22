35427ab1aa4eeb76b859d4ca300aa012855357cb
yoyoooo0903 <107978517+yoyoooo0903@users.noreply.github.com>
2022-06-22 00:30:11 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0dca759ba6c24e60af5934977429a800',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Hi',
+  domain: 'https://www.notion.so/yoyoooo/Hey-I-m-Youqi-0dca759ba6c24e60af5934977429a800',
+  author: 'yoyoooo',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
